fdf76d82d7e31459e5af0cdbcd5d1a9219f4d5d7
Michael Singh <86553650+minermikey@users.noreply.github.com>
2025-05-02 23:52:02 +0200

    Update UserInformation.kt

--- app/src/main/java/com/example/andriod3c/UserDOA/UserInformation.kt
+++ app/src/main/java/com/example/andriod3c/UserDOA/UserInformation.kt
@@ -2,7 +2,7 @@ package com.example.andriod3c.UserDOA
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+// this entity will allow for user ifnormation to be stored 
 @Entity
 data class UserInformation(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
